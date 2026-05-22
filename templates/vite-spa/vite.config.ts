36a6274381dfd746d23d8ec5ea42fc4c6e4d7861
Sam Huynh <samhwang2112.dev@gmail.com>
2026-05-23 08:08:34 +1000

    templates: update package versions and pnpm

--- templates/vite-spa/vite.config.ts
+++ templates/vite-spa/vite.config.ts
@@ -1,16 +1,16 @@
 /// <reference types="vite/client" />
 
-import { tanstackRouter } from '@tanstack/router-vite-plugin';
-import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
+import { tanstackRouter } from "@tanstack/router-vite-plugin";
+import babel from "@rolldown/plugin-babel";
+import react, { reactCompilerPreset } from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    react({
-      babel: {
-        plugins: [['babel-plugin-react-compiler']],
-      },
+    react(),
+    babel({
+      presets: [reactCompilerPreset()],
     }),
     tanstackRouter(),
   ],
