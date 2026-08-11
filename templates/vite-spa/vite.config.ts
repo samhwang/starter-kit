99bf7561df0fca8ce64897d3d8220ebac27ecc22
Sam Huynh <samhwang2112.dev@gmail.com>
2026-08-11 12:24:56 +1000

    vite-spa: add missing tanstack devtools dependencies

--- templates/vite-spa/vite.config.ts
+++ templates/vite-spa/vite.config.ts
@@ -4,6 +4,7 @@ import { tanstackRouter } from "@tanstack/router-vite-plugin";
 import babel from "@rolldown/plugin-babel";
 import react, { reactCompilerPreset } from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import { devtools } from "@tanstack/devtools-vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -13,5 +14,6 @@ export default defineConfig({
       presets: [reactCompilerPreset()],
     }),
     tanstackRouter(),
+    devtools(),
   ],
 });
