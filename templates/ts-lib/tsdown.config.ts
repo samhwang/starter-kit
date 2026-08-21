ce4c6e8dd44ddfde66af2b362c459fae9f687153
Sam Huynh <samhwang2112.dev@gmail.com>
2026-08-21 22:24:08 +1000

    build(ts): remove exporting CJS

--- templates/ts-lib/tsdown.config.ts
+++ templates/ts-lib/tsdown.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'tsdown';
 export default defineConfig({
   entry: ['src/index.ts'],
   outDir: 'dist',
-  format: ['cjs', 'esm'],
+  format: ['esm'],
   dts: true,
   sourcemap: true,
   clean: true,
