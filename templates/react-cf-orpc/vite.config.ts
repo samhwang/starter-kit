da153030539e658df0614ccb997beb998bf83433
Sam Huynh <samhwang2112.dev@gmail.com>
2026-08-27 14:05:23 +1000

    feat: normalize vite config for spa templates

--- templates/react-cf-orpc/vite.config.ts
+++ templates/react-cf-orpc/vite.config.ts
@@ -1,12 +1,21 @@
-/// <reference types="vitest" />
 /// <reference types="vite/client" />
 
 import { cloudflare } from '@cloudflare/vite-plugin';
+import babel from '@rolldown/plugin-babel';
+import { devtools } from '@tanstack/devtools-vite';
 import { tanstackRouter } from '@tanstack/router-vite-plugin';
-import react from '@vitejs/plugin-react';
+import react, { reactCompilerPreset } from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tanstackRouter(), ...(process.env.VITEST ? [] : [cloudflare()])],
+  plugins: [
+    tanstackRouter({ target: 'react', autoCodeSplitting: true }),
+    react(),
+    babel({
+      presets: [reactCompilerPreset()],
+    }),
+    devtools(),
+    ...(process.env.VITEST ? [] : [cloudflare()]),
+  ],
 });
--- templates/react-spa/vite.config.ts
+++ templates/react-spa/vite.config.ts
@@ -9,11 +9,11 @@ import { defineConfig } from 'vite';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
+    tanstackRouter({ target: 'react', autoCodeSplitting: true }),
     react(),
     babel({
       presets: [reactCompilerPreset()],
     }),
-    tanstackRouter(),
     devtools(),
   ],
 });
