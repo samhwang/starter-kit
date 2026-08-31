91fcfcdd2136ced57d3a0f3d4078e648f2958cf8
Sam Huynh <samhwang2112.dev@gmail.com>
2026-08-31 21:26:11 +1000

    test(tanstart): setup test projects

--- templates/react-tanstack-start/vitest.config.ts
+++ templates/react-tanstack-start/vitest.config.ts
@@ -2,10 +2,6 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
-    include: ['src/**/*.test.ts'],
-    environment: 'node',
-    globalSetup: ['./test/globalSetup.ts'],
-    setupFiles: ['./test/per-file-db.ts'],
     coverage: {
       provider: 'v8',
       include: ['src/**/*.{ts,tsx}'],
@@ -14,14 +10,40 @@ export default defineConfig({
         'test/**',
         'src/**/*.test.{ts,tsx}',
         'src/**/generated/**',
+        // Server function wrapeprs
+        'src/config/server/getSiteName.ts',
+        // Single line library wrapeprs
         'src/auth/client.ts',
         'src/auth/server.ts',
         'src/auth/server/lib.ts',
-        'src/config/server/getSiteName.ts',
         'src/database/lib/client.ts',
+        // Route pages
         'src/routes/**/*.tsx',
+        // API Routes handlers
         'src/routes/api/**',
       ],
     },
+    projects: [
+      {
+        extends: true,
+        test: {
+          name: 'ui-components',
+          include: ['src/**/*.test.tsx', 'src/**/hooks/**/*.test.ts'],
+          exclude: ['src/routes/**'],
+          environment: 'jsdom',
+        },
+      },
+      {
+        extends: true,
+        test: {
+          name: 'server',
+          include: ['src/**/*.test.ts'],
+          exclude: ['src/routes/**', 'src/**/hooks/**'],
+          environment: 'node',
+          globalSetup: ['./test/globalSetup.ts'],
+          setupFiles: ['./test/per-file-db.ts'],
+        },
+      },
+    ]
   },
 });
